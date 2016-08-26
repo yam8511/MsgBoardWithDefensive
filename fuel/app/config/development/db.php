ca3b0e90a402d4a6da57c7cb9f50e75429395def
Zoular <zoular.li@gmail.com>
2016-08-26 17:36:39 +0800

    refresh

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,7 +6,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=test',
+			'dsn'        => 'mysql:host=localhost;dbname=FuelphpMsgboard',
 			'username'   => 'yam8511',
 			'password'   => 'a7319779',
 		),
